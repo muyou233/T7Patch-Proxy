3c81d813008c784fc63d3b4db41c31fd0c040936
Scroptss <97276497+Scroptss@users.noreply.github.com>
2026-09-12 02:32:24 -0500

    Update framework.h

--- framework.h
+++ framework.h
@@ -139,7 +139,7 @@ inline std::vector<std::string> legit_packets = {
 #define CRASH_LOG_NAME "crashes.log"
 #define PATCH_CONFIG_LOCATION "t7patch.conf"
 #define ZBR_WINDOW_TEXT "Call of Duty: Black Ops III (community patch by serious)"
-#define ZBR_VERSION_FULL "Patch 3.05 - by serious <3"
+#define ZBR_VERSION_FULL "Patch 3.06 - by serious <3"
 #define SPOOF_UNLOCK_ALL false
 #define SPOOF_SKIP_CWL false
 #define SPOOF_GUM_COUNT 255
